ec7f3468ccac92752f4ef865f5d1ae5a67b75942
umarbinmusa <umarmusa20002@gmail.com>
2026-08-19 00:08:21 +0100

    commit client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -14,7 +14,7 @@ import {
   clearCustomerTokens,
 } from "../utils/customerTokenStorage.js";
 
-const GRAPHQL_URL = import.meta.env.VITE_GRAPHQL_URL || "https://inventory-api-x778.onrender.com/";
+const GRAPHQL_URL = import.meta.env.VITE_GRAPHQL_URL || "https://inventory-api-x778.onrender.com/graphql";
 
 const httpLink = new HttpLink({ uri: GRAPHQL_URL });
 
